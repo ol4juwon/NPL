010cfebe2f775063955d6203a8fe29992ff3a2d1
ola juwon <olajuwonlawal@icloud.com>
2021-02-04 13:39:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The No.1 Nigerian real estate and property website with property in Nigeria for sale rent and lease. We offer Nigerian property seekers an easy way to find details of property in Nigeria including homes, houses, lands, shops, office spaces and other commercial properties to buy or rent. Thousands of property listings available">
+    <meta name="robots" content="index, follow"/>
+    <link href="css/default.css" rel="stylesheet">
+
+<?php
+
+
+?>
+  </head>
+  <body>
+<div class="header">
+    <div class="nav">
+        <div class="nav-logo">
+            <img src="" alt="">
+        </div>
+    </div>
+    <div class="spotlight floater-img">
+        <div class="welcome">
+            Welcome To Nigeria Property Link 
+        </div>
+        <div class="tagline">
+            your dream porperty is just a search away...
+        </div>
+    </div>
+    <div class="search-home">
+
+
+    </div>
+
+
+
+</div>
+
+
+  </body>
+</html>
