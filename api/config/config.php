c83bf56d0db22aa16334c8c6e609f52b59e74635
ola juwon <olajuwonlawal@icloud.com>
2021-02-18 13:19:08 +0100

    sql user

--- api/config/config.php
+++ api/config/config.php
@@ -1,7 +1,7 @@
 <?php 
     $db_host = "localhost";
-    $db_name = "nightems_NPL";
-    $user = "nightems_ppl";
+    $db_name = "nigehtms_NPL";
+    $user = "nigehtms_ppl";
     $pword = "0L4kunle";
     $conn;
    
