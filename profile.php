d12c3d6d478a75ef20fc69a6c300f14fae7884e9
ola juwon <olajuwonlawal@icloud.com>
2021-02-20 20:37:40 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,48 @@
 <?php 
 
-if(isset($_SESSION['access_token'])){
-echo $_SESSION['user_first_name'];
-}else{
-    header("Location: register.php");
-}
+if(isset($_GET['code'])){
+    $token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
+    
+    if(!isset($token['error'])){
+        
 
+        try{
+        $gClient->setAccessToken($token['access_token']);
+
+        $_SESSION['access_token'] = $token['access_token'];
+        $google_Service = new Google_Service_Oauth2($gClient);
+
+        $data = $google_Service->userinfo->get();
+        
+        
+        }catch(Exception $e){
+            
+        }
+        
+        if(!empty($data['given_name'])){
+
+            $_SESSION['user_first_name'] = $data['given_name'];
+
+        }
+        if(!empty($data['family_name'])){
+            $_SESSION['user_last_name'] = $data['family_name'];
+
+        }
+        if(!empty($data['email'])){
+            $_SESSION['user_email_address'] = $data['email'];
+
+        }
+        if(!empty($data['picture'])){
+            $_SESSION['user_image'] = $data['picture'];
+
+        }
+
+
+
+
+    }
+
+header("Location: profile.php");
+
+}
 ?>
